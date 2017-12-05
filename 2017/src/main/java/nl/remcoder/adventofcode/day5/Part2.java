8786dc438c064aa1d9fd76a659ef8eb7000f090b
remco <remco.siemonsma@cgi.com>
2017-12-05 10:02:15 +0100

    Finished day 5

---
+++ 2017/src/main/java/nl/remcoder/adventofcode/day5/Part2.java
@@ -0,0 +1,34 @@
+package nl.remcoder.adventofcode.day5;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Part2 {
+    public static void main(String[] args) throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("day5/input").toURI()));
+
+        int[] instructions = new int[lines.size()];
+
+        for (int i = 0; i < lines.size(); i++) {
+            instructions[i] = Integer.parseInt(lines.get(i));
+        }
+
+        int iterations = 0;
+        int pc = 0;
+
+        while (0 <= pc && pc < instructions.length) {
+            int instruction = instructions[pc];
+
+            if(instruction >= 3) {
+                instructions[pc]--;
+            } else {
+                instructions[pc]++;
+            }
+            pc += instruction;
+            iterations++;
+        }
+
+        System.out.println(iterations);
+    }
+}
