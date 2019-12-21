b49edcd13fd58da0a7b1ca59be4ee25b128bda2c
Remco Siemonsma <remco.siemonsma@cgi.com>
2019-12-21 15:25:49 +0100

    Day 8 finished

---
+++ 2019/src/test/java/nl/remcoder/adventofcode/Day8Test.java
@@ -0,0 +1,54 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Day8Test {
+    private Day8 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day8();
+    }
+
+    @Test
+    void testPart1Case1() {
+        Stream<String> input = Stream.of("123456789012");
+
+        assertEquals(1, testSubject.handlePart1(input, 3, 2, 0, 1, 2));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        Stream<String> input = Files.lines(Paths.get(ClassLoader.getSystemResource("day8/input").toURI()));
+
+        assertEquals(1064, testSubject.handlePart1(input, 25, 6, 0, 1, 2));
+    }
+
+    @Test
+    void testPart2Case2() {
+        Stream<String> input = Stream.of("0222112222120000");
+
+        assertEquals("01\n10\n", testSubject.handlePart2(input, 2, 2, 0, 1, 2));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        Stream<String> input = Files.lines(Paths.get(ClassLoader.getSystemResource("day8/input").toURI()));
+String expectedResult = """
+1110011110011000110010010
+1001010000100101001010100
+1001011100100001001011000
+1110010000100001111010100
+1000010000100101001010100
+1000010000011001001010010
+""";
+        assertEquals(expectedResult, testSubject.handlePart2(input, 25, 6, 0, 1, 2));
+    }
+}
