228f1cc920613df21f7a12730ef9717aa751cb96
Remco Siemonsma <remco.siemonsma@nxp.com>
2020-12-02 11:40:11 +0100

    Finished day 2

---
+++ 2020/src/test/java/nl/remcoder/adventofcode/Day2Test.java
@@ -0,0 +1,44 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day2Test {
+
+    private Day2 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day2();
+    }
+
+    @Test
+    public void testPart1Case1() {
+        Stream<String> input = Stream.of("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc");
+
+        assertEquals(2, testSubject.handlePart1(input));
+    }
+
+    @Test
+    public void testPart1Input() throws Exception {
+        assertEquals(548, testSubject.handlePart1(Files.lines(Paths.get(ClassLoader.getSystemResource("day2/input").toURI()))));
+    }
+
+    @Test
+    public void testPart2Case1() {
+        Stream<String> input = Stream.of("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc");
+
+        assertEquals(1, testSubject.handlePart2(input));
+    }
+
+    @Test
+    public void testPart2Input() throws Exception {
+        assertEquals(502, testSubject.handlePart2(Files.lines(Paths.get(ClassLoader.getSystemResource("day2/input").toURI()))));
+    }
+}
