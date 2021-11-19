9c13f100c509f64433031277fc968fa33eff1902
Remco Siemonsma <remco@remcoder.nl>
2021-11-19 16:15:41 +0100

    Finished 2016 day 1 (again)

---
+++ 2016/src/test/java/nl/remcoder/adventofcode/Day1Test.java
@@ -0,0 +1,59 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class Day1Test {
+    private Day1 testSubject;
+
+    @Before
+    public void setUp() {
+        testSubject = new Day1();
+    }
+
+    @Test
+    public void part1Case1() {
+        String data = "R2, L3";
+
+        assertEquals(5, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    public void part1Case2() {
+        String data = "R2, R2, R2";
+
+        assertEquals(2, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    public void part1Case3() {
+        String data = "R5, L5, R5, R3";
+
+        assertEquals(12, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    public void testPart1Input() throws Exception {
+        assertEquals(239, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day1/input").toURI()))));
+    }
+
+    @Test
+    public void part2Case1() {
+        String data = "R8, R4, R4, R8";
+
+        assertEquals(4, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    public void testPart2Input() throws Exception {
+        assertEquals(141, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day1/input").toURI()))));
+    }
+}
