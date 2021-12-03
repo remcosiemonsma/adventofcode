b4e6ad5ce457e542dd267bfd37f2a905a9804e46
Remco Siemonsma <remco@remcoder.nl>
2021-12-03 08:33:14 +0100

    Finished 2021 day 3

---
+++ 2021/src/test/java/nl/remcoder/adventofcode/Day3Test.java
@@ -0,0 +1,71 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day3Test {
+    private Day3 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day3();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String data = """
+                      00100
+                      11110
+                      10110
+                      10111
+                      10101
+                      01111
+                      00111
+                      11100
+                      10000
+                      11001
+                      00010
+                      01010
+                      """;
+
+        assertEquals(198, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(3959450, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day3/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String data = """
+                      00100
+                      11110
+                      10110
+                      10111
+                      10101
+                      01111
+                      00111
+                      11100
+                      10000
+                      11001
+                      00010
+                      01010
+                      """;
+
+        assertEquals(230, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(7440311, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day3/input").toURI()))));
+    }
+}
