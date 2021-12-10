92d0f47444690869d60a20f14e7b306ebeeb1af9
Remco Siemonsma <remco@remcoder.nl>
2021-12-10 10:12:08 +0100

    Finished 2021 day 10

---
+++ 2021/src/test/java/nl/remcoder/adventofcode/Day10Test.java
@@ -0,0 +1,67 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day10Test {
+    private Day10 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day10();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String data = """
+                      [({(<(())[]>[[{[]{<()<>>
+                      [(()[<>])]({[<{<<[]>>(
+                      {([(<{}[<>[]}>{[]{[(<()>
+                      (((({<>}<{<{<>}{[]{[]{}
+                      [[<[([]))<([[{}[[()]]]
+                      [{[{({}]{}}([{[{{{}}([]
+                      {<[[]]>}<{[{[{[]{()[[[]
+                      [<(<(<(<{}))><([]([]()
+                      <{([([[(<>()){}]>(<<{{
+                      <{([{{}}[<[[[<>{}]]]>[]]
+                      """;
+
+        assertEquals(26397, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(394647, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day10/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String data = """
+                      [({(<(())[]>[[{[]{<()<>>
+                      [(()[<>])]({[<{<<[]>>(
+                      {([(<{}[<>[]}>{[]{[(<()>
+                      (((({<>}<{<{<>}{[]{[]{}
+                      [[<[([]))<([[{}[[()]]]
+                      [{[{({}]{}}([{[{{{}}([]
+                      {<[[]]>}<{[{[{[]{()[[[]
+                      [<(<(<(<{}))><([]([]()
+                      <{([([[(<>()){}]>(<<{{
+                      <{([{{}}[<[[[<>{}]]]>[]]
+                      """;
+
+        assertEquals(288957, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(2380061249L, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day10/input").toURI()))));
+    }
+}
