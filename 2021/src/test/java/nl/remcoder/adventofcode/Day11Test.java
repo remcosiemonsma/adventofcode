58150bc59a4e218dee2716ebf1dc9250a3886085
Remco Siemonsma <remco@remcoder.nl>
2021-12-11 11:25:14 +0100

    Finished 2021 day 11

---
+++ 2021/src/test/java/nl/remcoder/adventofcode/Day11Test.java
@@ -0,0 +1,67 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day11Test {
+    private Day11 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day11();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String data = """
+                      5483143223
+                      2745854711
+                      5264556173
+                      6141336146
+                      6357385478
+                      4167524645
+                      2176841721
+                      6882881134
+                      4846848554
+                      5283751526
+                      """;
+
+        assertEquals(1656, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(1659, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day11/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String data = """
+                      5483143223
+                      2745854711
+                      5264556173
+                      6141336146
+                      6357385478
+                      4167524645
+                      2176841721
+                      6882881134
+                      4846848554
+                      5283751526
+                      """;
+
+        assertEquals(195, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(227, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day11/input").toURI()))));
+    }
+}
