2e50ef47ed4e3284cdf0e4252080269dea2b198f
Remco Siemonsma <remcosiemonsma@dedicon.nl>
2021-12-16 12:09:07 +0100

    Finished 2021 day 16

---
+++ 2021/src/test/java/nl/remcoder/adventofcode/Day16Test.java
@@ -0,0 +1,148 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day16Test {
+    private Day16 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day16();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String data = """
+                      D2FE28
+                      """;
+
+        assertEquals(6, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Case2() {
+        String data = """
+                      8A004A801A8002F478
+                      """;
+
+        assertEquals(16, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Case3() {
+        String data = """
+                      620080001611562C8802118E34
+                      """;
+
+        assertEquals(12, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Case4() {
+        String data = """
+                      C0015000016115A2E0802F182340
+                      """;
+
+        assertEquals(23, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Case5() {
+        String data = """
+                      A0016C880162017C3686B18A3D4780
+                      """;
+
+        assertEquals(31, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(984, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day16/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String data = """
+                      C200B40A82
+                      """;
+
+        assertEquals(3, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case2() {
+        String data = """
+                      04005AC33890
+                      """;
+
+        assertEquals(54, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case3() {
+        String data = """
+                      880086C3E88112
+                      """;
+
+        assertEquals(7, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case5() {
+        String data = """
+                      CE00C43D881120
+                      """;
+
+        assertEquals(9, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case6() {
+        String data = """
+                      D8005AC2A8F0
+                      """;
+
+        assertEquals(1, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case7() {
+        String data = """
+                      F600BC2D8F
+                      """;
+
+        assertEquals(0, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case8() {
+        String data = """
+                      9C005AC2F8F0
+                      """;
+
+        assertEquals(0, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Case9() {
+        String data = """
+                      9C0141080250320F1802104A08
+                      """;
+
+        assertEquals(1, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(1015320896946L, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day16/input").toURI()))));
+    }
+}
