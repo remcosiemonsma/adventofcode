747d6df8ecad9964239afcac2b99848ad5239b14
Remco Siemonsma <remcosiemonsma@dedicon.nl>
2021-12-20 11:43:13 +0100

    Finished 2021 day 20

---
+++ 2021/src/test/java/nl/remcoder/adventofcode/Day20Test.java
@@ -0,0 +1,61 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day20Test {
+    private Day20 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day20();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String data = """
+                      ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+                      
+                      #..#.
+                      #....
+                      ##..#
+                      ..#..
+                      ..###
+                      """;
+
+        assertEquals(35, testSubject.handlePart1(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(5081, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day20/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String data = """
+                      ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+                      
+                      #..#.
+                      #....
+                      ##..#
+                      ..#..
+                      ..###
+                      """;
+
+        assertEquals(3351, testSubject.handlePart2(Arrays.stream(data.split("\n"))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(15088, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day20/input").toURI()))));
+    }
+}
