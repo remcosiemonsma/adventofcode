925776e04afc4b0f66364197a9b2db82f567fa2e
Remco <remco.siemonsma@gmail.com>
2022-02-03 14:39:18 +0100

    Finished 2015 day 19

---
+++ 2015/src/test/java/nl/remcoder/adventofcode/Day19Test.java
@@ -0,0 +1,56 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day19Test {
+    private Day19 testSubject;
+
+    @BeforeEach
+    public void setUp() {
+        testSubject = new Day19();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       H => HO
+                       H => OH
+                       O => HH
+                                       
+                       HOH
+                       """;
+
+        assertEquals(4, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Case2() {
+        String input = """
+                       H => HO
+                       H => OH
+                       O => HH
+                                       
+                       HOHOHO
+                       """;
+
+        assertEquals(7, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(509, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(195, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+}
