2892827fa7a076b79b6437149abb14e94b33b2bb
Remco <remco.siemonsma@gmail.com>
2022-02-04 15:09:02 +0100

    Finished 2015 day 21

---
+++ 2015/src/test/java/nl/remcoder/adventofcode/Day21Test.java
@@ -0,0 +1,30 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day21Test {
+    private Day21 testSubject;
+
+    @BeforeEach
+    public void setUp() {
+        testSubject = new Day21();
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(91, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day21/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(158, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day21/input").toURI()))));
+    }
+}
---
+++ library/src/main/java/nl/remcoder/adventofcode/library/pathfinding/Node.java
@@ -0,0 +1,16 @@
+package nl.remcoder.adventofcode.library.pathfinding;
+
+import java.util.Map;
+
+public interface Node extends Comparable<Node> {
+    Map<? extends Node, Integer> getNeighbors();
+    int getDistance();
+    boolean isVisited();
+    void setVisited(boolean visited);
+    void setDistance(int distance);
+
+    @Override
+    default int compareTo(Node o) {
+        return Integer.compare(this.getDistance(), o.getDistance());
+    }
+}
