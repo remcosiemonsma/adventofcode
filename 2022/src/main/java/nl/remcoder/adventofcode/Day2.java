01898d4d9c425bc371376aedcbd84618764ee25f
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-02 08:54:28 +0100

    Finished 2022 day 2

---
+++ 2022/src/main/java/nl/remcoder/adventofcode/Day2.java
@@ -0,0 +1,92 @@
+package nl.remcoder.adventofcode;
+
+import java.util.stream.Stream;
+
+public class Day2 {
+
+    public int handlePart1(Stream<String> input) {
+        return input.mapToInt(this::mapToScorePart1)
+                    .sum();
+    }
+
+    public int handlePart2(Stream<String> input) {
+        return input.mapToInt(this::mapToScorePart2)
+                    .sum();
+    }
+
+    private int mapToScorePart1(String line) {
+        RockPaperScissors elf = switch (line.charAt(0)) {
+            case 'A' -> RockPaperScissors.ROCK;
+            case 'B' -> RockPaperScissors.PAPER;
+            case 'C' -> RockPaperScissors.SCISSORS;
+            default -> throw new AssertionError("Eek!");
+        };
+        RockPaperScissors player = switch (line.charAt(2)) {
+            case 'X' -> RockPaperScissors.ROCK;
+            case 'Y' -> RockPaperScissors.PAPER;
+            case 'Z' -> RockPaperScissors.SCISSORS;
+            default -> throw new AssertionError("Eek!");
+        };
+        return switch (player) {
+            case ROCK -> switch (elf) {
+                case ROCK -> 4;
+                case PAPER -> 1;
+                case SCISSORS -> 7;
+            };
+            case PAPER -> switch (elf) {
+                case ROCK -> 8;
+                case PAPER -> 5;
+                case SCISSORS -> 2;
+            };
+            case SCISSORS -> switch (elf) {
+                case ROCK -> 3;
+                case PAPER -> 9;
+                case SCISSORS -> 6;
+            };
+        };
+    }
+
+    private int mapToScorePart2(String line) {
+        RockPaperScissors elf = switch (line.charAt(0)) {
+            case 'A' -> RockPaperScissors.ROCK;
+            case 'B' -> RockPaperScissors.PAPER;
+            case 'C' -> RockPaperScissors.SCISSORS;
+            default -> throw new AssertionError("Eek!");
+        };
+        Result result = switch (line.charAt(2)) {
+            case 'X' -> Result.LOSS;
+            case 'Y' -> Result.DRAW;
+            case 'Z' -> Result.WIN;
+            default -> throw new AssertionError("Eek!");
+        };
+        return switch (result) {
+            case WIN -> switch (elf) {
+                case ROCK -> 8;
+                case SCISSORS -> 7;
+                case PAPER -> 9;
+            };
+            case DRAW -> switch (elf) {
+                case ROCK -> 4;
+                case SCISSORS -> 6;
+                case PAPER -> 5;
+            };
+            case LOSS -> switch (elf) {
+                case ROCK -> 3;
+                case SCISSORS -> 2;
+                case PAPER -> 1;
+            };
+        };
+    }
+
+    private enum RockPaperScissors {
+        ROCK,
+        PAPER,
+        SCISSORS
+    }
+
+    private enum Result {
+        LOSS,
+        WIN,
+        DRAW
+    }
+}
