29a890b850b506e93c6cf644a13f0a370a844032
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-02 08:54:28 +0100

    Finished 2022 day 2

---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day2Test.java
@@ -0,0 +1,52 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day2Test {
+    private Day2 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day2();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       A Y
+                       B X
+                       C Z
+                       """;
+
+        assertEquals(15, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(8933, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day2/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       A Y
+                       B X
+                       C Z
+                       """;
+
+        assertEquals(12, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(11998, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day2/input").toURI()))));
+    }
+}
