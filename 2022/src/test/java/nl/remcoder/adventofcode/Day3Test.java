bc16977f4aa238be10ade93a0f3f8124ae331d9c
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-03 07:59:11 +0100

    Finished 2022 day 3

---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day3Test.java
@@ -0,0 +1,58 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day3Test {
+    private Day3 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day3();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       vJrwpWtwJgWrhcsFMMfFFhFp
+                       jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                       PmmdzqPrVvPwwTWBwg
+                       wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                       ttgJtRGJQctTZtZT
+                       CrZsJsPPZsGzwwsLwLmpwMDw
+                       """;
+
+        assertEquals(157, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(7845, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day3/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       vJrwpWtwJgWrhcsFMMfFFhFp
+                       jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                       PmmdzqPrVvPwwTWBwg
+                       wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                       ttgJtRGJQctTZtZT
+                       CrZsJsPPZsGzwwsLwLmpwMDw
+                       """;
+
+        assertEquals(70, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(2790, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day3/input").toURI()))));
+    }
+}
