f9cb2860144bd2fc527ba1b0a0adf402eccaf536
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-14 10:21:56 +0100

    Finished 2022 day 14

---
+++ 2022/src/main/java/nl/remcoder/adventofcode/Day14.java
@@ -0,0 +1,128 @@
+package nl.remcoder.adventofcode;
+
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+import nl.remcoder.adventofcode.library.model.Coordinate;
+import nl.remcoder.adventofcode.library.model.Grid;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day14 implements AdventOfCodeSolution<Long> {
+    @Override
+    public Long handlePart1(Stream<String> input) {
+        var grid = parseGrid(input, 0);
+
+        simulateSand(grid);
+
+        return grid.countElements('o');
+    }
+
+    @Override
+    public Long handlePart2(Stream<String> input) {
+        var grid = parseGrid(input, 2);
+        
+        for (int x = grid.getStartx(); x <= grid.getEndx(); x++) {
+            grid.set(new Coordinate(x, grid.getEndy()), '#');
+        }
+
+        simulateSand(grid);
+        
+        return grid.countElements('o');
+    }
+
+    private void simulateSand(Grid<Character> grid) {
+        var finished = false;
+
+        while (!finished) {
+            var sand = new Coordinate(500, 0);
+            if (grid.get(sand) == 'o') {
+                break;
+            }
+            var falling = true;
+            while (falling) {
+                var below = grid.get(sand.below());
+                var belowLeft = grid.get(sand.bottomLeft());
+                var belowRight = grid.get(sand.bottomRight());
+
+                if (below != null && below == '.') {
+                    sand = sand.below();
+                    continue;
+                } else if (belowLeft != null && belowLeft == '.') {
+                    sand = sand.bottomLeft();
+                    continue;
+                } else if (belowRight != null && belowRight == '.') {
+                    sand = sand.bottomRight();
+                    continue;
+                }
+
+                if (below == null) {
+                    finished = true;
+                    break;
+                } else if (belowLeft == null) {
+                    finished = true;
+                    break;
+                } else if (belowRight == null) {
+                    finished = true;
+                    break;
+                }
+
+                grid.set(sand, 'o');
+                falling = false;
+            }
+        }
+    }
+
+    private Grid<Character> parseGrid(Stream<String> input, int offset) {
+        var walls = input.map(this::parseToCoordinates).toList();
+
+        var maxX = walls.stream().mapToInt(wall -> wall.stream()
+                                                       .mapToInt(Coordinate::x)
+                                                       .max()
+                                                       .orElseThrow(() -> new AssertionError("Eek!")))
+                        .max()
+                        .orElseThrow(() -> new AssertionError("Ook!")) + 512;
+
+        var minX = walls.stream().mapToInt(wall -> wall.stream()
+                                                       .mapToInt(Coordinate::x)
+                                                       .min()
+                                                       .orElseThrow(() -> new AssertionError("Eek!")))
+                        .min()
+                        .orElseThrow(() -> new AssertionError("Ook!")) - 512;
+
+        var maxY = walls.stream().mapToInt(wall -> wall.stream()
+                                                       .mapToInt(Coordinate::y)
+                                                       .max()
+                                                       .orElseThrow(() -> new AssertionError("Eek!")))
+                        .max()
+                        .orElseThrow(() -> new AssertionError("Ook!")) + offset;
+
+        var minY = 0;
+
+        var grid = new Grid<Character>(minX, minY, maxX, maxY);
+
+        grid.fill('.');
+
+        for (var wall : walls) {
+            for (int i = 0; i < wall.size() - 1; i++) {
+                Coordinate start = wall.get(i);
+                Coordinate end = wall.get(i + 1);
+
+                start.getAllBetween(end)
+                     .forEach(coordinate -> grid.set(coordinate, '#'));
+            }
+        }
+        return grid;
+    }
+
+    private List<Coordinate> parseToCoordinates(String line) {
+        return Arrays.stream(line.split(" -> "))
+                     .map(this::parseToCoordinate)
+                     .toList();
+    }
+
+    private Coordinate parseToCoordinate(String part) {
+        String[] split = part.split(",");
+        return new Coordinate(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+    }
+}
---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day14Test.java
@@ -0,0 +1,50 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day14Test {
+    private Day14 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day14();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       498,4 -> 498,6 -> 496,6
+                       503,4 -> 502,4 -> 502,9 -> 494,9
+                       """;
+
+        assertEquals(24, testSubject.handlePart1(input.lines()));
+    }
+    
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(828, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day14/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       498,4 -> 498,6 -> 496,6
+                       503,4 -> 502,4 -> 502,9 -> 494,9
+                       """;
+
+        assertEquals(93, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(25500, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day14/input").toURI()))));
+    }
+}
