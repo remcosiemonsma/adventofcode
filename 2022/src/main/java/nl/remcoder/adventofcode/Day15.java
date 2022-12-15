8bd6daa28a49a0f2f07b163cc378e1818007db0a
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-15 11:43:45 +0100

    Finished 2022 day 15

---
+++ 2022/src/main/java/nl/remcoder/adventofcode/Day15.java
@@ -0,0 +1,135 @@
+package nl.remcoder.adventofcode;
+
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+import nl.remcoder.adventofcode.library.model.Coordinate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Day15 implements AdventOfCodeSolution<Long> {
+    private static final Pattern SENSOR_PATTERN =
+            Pattern.compile("Sensor at x=(-?\\d*), y=(-?\\d*): closest beacon is at x=(-?\\d*), y=(-?\\d*)");
+    public int rowToExamine = 2000000;
+
+    @Override
+    public Long handlePart1(Stream<String> input) {
+        var sensors = input.map(SENSOR_PATTERN::matcher)
+                           .filter(Matcher::matches)
+                           .map(this::mapToSensor)
+                           .filter(sensor -> sensor.isRowInRange(rowToExamine))
+                           .toList();
+
+        return sensors.stream()
+                      .map(sensor -> sensor.getCoverageInRow(rowToExamine))
+                      .flatMap(Collection::stream)
+                      .distinct()
+                      .filter(coordinate -> sensors.stream().map(Sensor::getBeacon)
+                                                   .noneMatch(beacon -> beacon.equals(coordinate)))
+                      .count();
+    }
+
+    @Override
+    public Long handlePart2(Stream<String> input) {
+        var sensors = input.map(SENSOR_PATTERN::matcher)
+                           .filter(Matcher::matches)
+                           .map(this::mapToSensor)
+                           .toList();
+
+        int expectedRows = rowToExamine * 2;
+
+        for (var row = expectedRows; row >= 0; row--) {
+            for (Sensor sensor : sensors) {
+                Coordinate left = sensor.getLeftOutsideRangeCoordinateOnRow(row);
+                Coordinate right = sensor.getRightOutsideRangeCoordinateOnRow(row);
+
+                if (left != null) {
+                    if (left.x() >= 0) {
+                        if (sensors.stream().noneMatch(sensor1 -> sensor1.isCoordinateInRange(left))) {
+                            return ((long) left.x() * 4000000L) + left.y();
+                        }
+                    }
+                }
+                if (right != null) {
+                    if (right.x() <= expectedRows) {
+                        if (sensors.stream().noneMatch(sensor1 -> sensor1.isCoordinateInRange(right))) {
+                            return ((long) right.x() * 4000000L) + right.y();
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Sensor mapToSensor(Matcher matcher) {
+        return new Sensor(new Coordinate(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2))),
+                          new Coordinate(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4))));
+    }
+
+    private static class Sensor {
+        private final Coordinate position;
+        private final Coordinate beacon;
+        private final int range;
+
+        private Sensor(Coordinate position, Coordinate beacon) {
+            this.position = position;
+            this.beacon = beacon;
+            range = position.getDistanceTo(beacon);
+        }
+
+        public Coordinate getBeacon() {
+            return beacon;
+        }
+
+        public boolean isRowInRange(int row) {
+            int minRow = position.y() - range;
+            int maxRow = position.y() + range;
+
+            return minRow <= row && row <= maxRow;
+        }
+
+        public List<Coordinate> getCoverageInRow(int row) {
+            int distanceToRow = Math.abs(position.y() - row);
+            int rangeInRow = range - distanceToRow;
+
+            var coordinates = new ArrayList<Coordinate>();
+
+            for (int x = position.x() - rangeInRow; x <= position.x() + rangeInRow; x++) {
+                coordinates.add(new Coordinate(x, row));
+            }
+
+            return coordinates;
+        }
+
+        public Coordinate getLeftOutsideRangeCoordinateOnRow(int row) {
+            int distanceToRow = Math.abs(position.y() - row);
+            int rangeInRow = range - distanceToRow;
+            
+            if (rangeInRow >= 0) {
+                return new Coordinate(position.x() - (rangeInRow + 1), row);
+            } else {
+                return null;
+            }
+        }
+        
+        public Coordinate getRightOutsideRangeCoordinateOnRow(int row) {
+            int distanceToRow = Math.abs(position.y() - row);
+            int rangeInRow = range - distanceToRow;
+            
+            if (rangeInRow >= 0) {
+                return new Coordinate(position.x() + (rangeInRow + 1), row);
+            } else {
+                return null;
+            }
+        }
+        
+        public boolean isCoordinateInRange(Coordinate coordinate) {
+            return position.getDistanceTo(coordinate) <= range;
+        }
+    }
+}
---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day15Test.java
@@ -0,0 +1,78 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day15Test {
+    private Day15 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day15();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                       Sensor at x=9, y=16: closest beacon is at x=10, y=16
+                       Sensor at x=13, y=2: closest beacon is at x=15, y=3
+                       Sensor at x=12, y=14: closest beacon is at x=10, y=16
+                       Sensor at x=10, y=20: closest beacon is at x=10, y=16
+                       Sensor at x=14, y=17: closest beacon is at x=10, y=16
+                       Sensor at x=8, y=7: closest beacon is at x=2, y=10
+                       Sensor at x=2, y=0: closest beacon is at x=2, y=10
+                       Sensor at x=0, y=11: closest beacon is at x=2, y=10
+                       Sensor at x=20, y=14: closest beacon is at x=25, y=17
+                       Sensor at x=17, y=20: closest beacon is at x=21, y=22
+                       Sensor at x=16, y=7: closest beacon is at x=15, y=3
+                       Sensor at x=14, y=3: closest beacon is at x=15, y=3
+                       Sensor at x=20, y=1: closest beacon is at x=15, y=3
+                       """;
+
+        testSubject.rowToExamine = 10;
+        
+        assertEquals(26, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(4737567, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day15/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                       Sensor at x=9, y=16: closest beacon is at x=10, y=16
+                       Sensor at x=13, y=2: closest beacon is at x=15, y=3
+                       Sensor at x=12, y=14: closest beacon is at x=10, y=16
+                       Sensor at x=10, y=20: closest beacon is at x=10, y=16
+                       Sensor at x=14, y=17: closest beacon is at x=10, y=16
+                       Sensor at x=8, y=7: closest beacon is at x=2, y=10
+                       Sensor at x=2, y=0: closest beacon is at x=2, y=10
+                       Sensor at x=0, y=11: closest beacon is at x=2, y=10
+                       Sensor at x=20, y=14: closest beacon is at x=25, y=17
+                       Sensor at x=17, y=20: closest beacon is at x=21, y=22
+                       Sensor at x=16, y=7: closest beacon is at x=15, y=3
+                       Sensor at x=14, y=3: closest beacon is at x=15, y=3
+                       Sensor at x=20, y=1: closest beacon is at x=15, y=3
+                       """;
+
+        testSubject.rowToExamine = 10;
+
+        assertEquals(56000011, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(13267474686239L, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day15/input").toURI()))));
+    }
+}
