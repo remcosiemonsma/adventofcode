678420bb929fa6005023d3b747bb95b379d1d3fc
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-18 15:35:51 +0100

    Finished 2022 day 18

---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day18Test.java
@@ -0,0 +1,82 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day18Test {
+    private Day18 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day18();
+    }
+
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       1,1,1
+                       2,1,1
+                       """;
+
+        assertEquals(10, testSubject.handlePart1(input.lines()));
+    }
+    
+    @Test
+    void testPart1Case2() {
+        String input = """
+                       2,2,2
+                       1,2,2
+                       3,2,2
+                       2,1,2
+                       2,3,2
+                       2,2,1
+                       2,2,3
+                       2,2,4
+                       2,2,6
+                       1,2,5
+                       3,2,5
+                       2,1,5
+                       2,3,5
+                       """;
+
+        assertEquals(64, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(3564, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day18/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       2,2,2
+                       1,2,2
+                       3,2,2
+                       2,1,2
+                       2,3,2
+                       2,2,1
+                       2,2,3
+                       2,2,4
+                       2,2,6
+                       1,2,5
+                       3,2,5
+                       2,1,5
+                       2,3,5
+                       """;
+
+        assertEquals(58, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(2106, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day18/input").toURI()))));
+    }
+}
---
+++ library/src/main/java/nl/remcoder/adventofcode/library/model/Coordinate3D.java
@@ -0,0 +1,39 @@
+package nl.remcoder.adventofcode.library.model;
+
+import java.util.Set;
+
+public record Coordinate3D(int x, int y, int z) {
+    
+    public Coordinate3D getLeft() {
+        return new Coordinate3D(x - 1, y, z);
+    }
+    
+    public Coordinate3D getRight() {
+        return new Coordinate3D(x + 1, y, z);
+    }
+    
+    public Coordinate3D getAbove() {
+        return new Coordinate3D(x, y + 1, z);
+    }
+    
+    public Coordinate3D getBelow() {
+        return new Coordinate3D(x, y - 1, z);
+    }
+    
+    public Coordinate3D getFront() {
+        return new Coordinate3D(x, y, z + 1);
+    }
+    
+    public Coordinate3D getRear() {
+        return new Coordinate3D(x, y, z - 1);
+    }
+    
+    public Set<Coordinate3D> getNeighbors() {
+        return Set.of(getLeft(), getRight(), getAbove(), getBelow(), getFront(), getRear());
+    }
+
+    @Override
+    public String toString() {
+        return x + "," + y + "," + z;
+    }
+}
