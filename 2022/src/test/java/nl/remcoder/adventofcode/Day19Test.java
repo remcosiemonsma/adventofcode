3f25f5aa66e9b0334c0e529f749e53ba77365c40
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-19 13:31:24 +0100

    Finished 2022 day 19

---
+++ 2022/src/test/java/nl/remcoder/adventofcode/Day19Test.java
@@ -0,0 +1,50 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day19Test {
+    private Day19 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day19();
+    }
+    
+    @Test
+    void testPart1Case1() {
+        String input = """
+                       Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+                       Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+                       """;
+
+        assertEquals(33, testSubject.handlePart1(input.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(1262, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Case1() {
+        String input = """
+                       Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+                       Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+                       """;
+
+        assertEquals(3472, testSubject.handlePart2(input.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(37191, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+}
