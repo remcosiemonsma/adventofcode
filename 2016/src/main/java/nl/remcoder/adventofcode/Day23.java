614e53bff25055a07d14912df881dca08900ac90
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-26 23:20:36 +0100

    Finished 2016 day 23

--- 2016/src/main/java/nl/remcoder/adventofcode/Day23.java
+++ 2016/src/main/java/nl/remcoder/adventofcode/Day23.java
@@ -5,13 +5,16 @@ import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
 
 import java.util.stream.Stream;
 
-public class Day12 implements AdventOfCodeSolution<Integer> {
+public class Day23 implements AdventOfCodeSolution<Integer> {
+
     @Override
     public Integer handlePart1(Stream<String> input) {
         var instructions = input.map(s -> s.split(" ")).toArray(String[][]::new);
-
+        
         CPU cpu = new CPU();
         
+        cpu.setRegistera(7);
+
         cpu.performOperations(instructions);
 
         return cpu.getRegistera();
@@ -22,8 +25,8 @@ public class Day12 implements AdventOfCodeSolution<Integer> {
         var instructions = input.map(s -> s.split(" ")).toArray(String[][]::new);
 
         CPU cpu = new CPU();
-        
-        cpu.setRegisterc(1);
+
+        cpu.setRegistera(12);
 
         cpu.performOperations(instructions);
 
---
+++ 2016/src/main/java/nl/remcoder/adventofcode/asembunny/CPU.java
@@ -0,0 +1,132 @@
+package nl.remcoder.adventofcode.asembunny;
+
+public class CPU {
+    private int registera = 0;
+    private int registerb = 0;
+    private int registerc = 0;
+    private int registerd = 0;
+
+    public int getRegistera() {
+        return registera;
+    }
+
+    public void setRegistera(int registera) {
+        this.registera = registera;
+    }
+
+    public int getRegisterb() {
+        return registerb;
+    }
+
+    public void setRegisterb(int registerb) {
+        this.registerb = registerb;
+    }
+
+    public int getRegisterc() {
+        return registerc;
+    }
+
+    public void setRegisterc(int registerc) {
+        this.registerc = registerc;
+    }
+
+    public int getRegisterd() {
+        return registerd;
+    }
+
+    public void setRegisterd(int registerd) {
+        this.registerd = registerd;
+    }
+
+    public void performOperations(String[][] instructions) {
+        var instructionCounter = 0;
+
+        while (instructionCounter < instructions.length) {
+            var parts = instructions[instructionCounter];
+
+            switch (parts[0]) {
+                case "cpy" -> {
+                    performCopyOperation(parts[1], parts[2]);
+                    instructionCounter++;
+                }
+                case "inc" -> {
+                    performIncrementOperation(parts[1]);
+                    instructionCounter++;
+                }
+                case "dec" -> {
+                    performDecrementOperation(parts[1]);
+                    instructionCounter++;
+                }
+                case "jnz" -> {
+                    int value = getValue(parts[1]);
+                    if (value != 0) {
+                        instructionCounter += getValue(parts[2]);
+                    } else {
+                        instructionCounter++;
+                    }
+                }
+                case "tgl" -> {
+                    int value = getValue(parts[1]);
+                    int index = instructionCounter + value;
+                    if (index > 0 && index < instructions.length) {
+                        String[] instruction = instructions[index];
+                        instruction[0] = switch (instruction[0]) {
+                            case "cpy" -> "jnz";
+                            case "inc" -> "dec";
+                            case "dec" -> "inc";
+                            case "jnz" -> "cpy";
+                            case "tgl" -> "inc";
+                            default -> throw new AssertionError("Eek!");
+                        };
+                    }
+                    instructionCounter++;
+                }
+            }
+        }
+    }
+
+    private void performDecrementOperation(String register) {
+        switch (register) {
+            case "a" -> registera--;
+            case "b" -> registerb--;
+            case "c" -> registerc--;
+            case "d" -> registerd--;
+        }
+    }
+
+    private void performIncrementOperation(String register) {
+        switch (register) {
+            case "a" -> registera++;
+            case "b" -> registerb++;
+            case "c" -> registerc++;
+            case "d" -> registerd++;
+        }
+    }
+
+    private void performCopyOperation(String sourceRegister, String targetRegister) {
+        var value = getValue(sourceRegister);
+        switch (targetRegister) {
+            case "a" -> registera = value;
+            case "b" -> registerb = value;
+            case "c" -> registerc = value;
+            case "d" -> registerd = value;
+        }
+    }
+
+    private int getValue(String register) {
+        var value = 0;
+
+        if (register.charAt(0) == '-' || Character.isDigit(register.charAt(0))) {
+            value = Integer.parseInt(register);
+        } else {
+            value = switch (register) {
+                case "a" -> registera;
+                case "b" -> registerb;
+                case "c" -> registerc;
+                case "d" -> registerd;
+                default -> value;
+            };
+        }
+        return value;
+    }
+}
