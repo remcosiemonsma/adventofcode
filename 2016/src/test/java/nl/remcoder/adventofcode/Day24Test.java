f90d4742e19924f83260e6810dfa88178dd79907
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-26 23:56:27 +0100

    Finished 2016 day 24

---
+++ 2016/src/test/java/nl/remcoder/adventofcode/Day24Test.java
@@ -0,0 +1,47 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day24Test {
+    private Day24 testSubject;
+
+    @BeforeEach
+    public void setUp() {
+        testSubject = new Day24();
+    }
+    
+    @Test
+    void part1Case1() {
+        String data = """
+                      ###########
+                      #0.1.....2#
+                      #.#######.#
+                      #4.......3#
+                      ###########
+                      """;
+
+        testSubject.setExpectedSize(5);
+
+        assertEquals(14, testSubject.handlePart1(data.lines()));
+    }
+    
+    @Test
+    void testPart1Input() throws Exception {
+        testSubject.setExpectedSize(8);
+        
+        assertEquals(442, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day24/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(660, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day24/input").toURI()))));
+    }
+}
