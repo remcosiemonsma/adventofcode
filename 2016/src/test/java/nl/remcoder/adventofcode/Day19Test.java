d25b7f984a812d9e3a49f81928197d670faeea28
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-26 18:43:09 +0100

    Finished 2016 day 19

---
+++ 2016/src/test/java/nl/remcoder/adventofcode/Day19Test.java
@@ -0,0 +1,57 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day19Test {
+    private Day19 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day19();
+    }
+
+    @Test
+    void part1Case1() {
+        String data = """
+                      5
+                      """;
+
+        assertEquals(3, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void part1Case2() {
+        String data = """
+                      6
+                      """;
+
+        assertEquals(5, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(1830117, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+
+    @Test
+    void part2Case1() {
+        String data = """
+                      5
+                      """;
+
+        assertEquals(2, testSubject.handlePart2(data.lines()));
+    }
+    
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(1417887, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day19/input").toURI()))));
+    }
+}
