5a1976264446ae53155dc402fd313f80cd0bc3b8
Remco Siemonsma <remco.siemonsma@sanoma.com>
2022-12-31 16:34:04 +0100

    Partial work on 2022 day 22, part 1 works, part 2 still gives incorrect answer

---
+++ 2018/src/test/java/nl/remcoder/adventofcode/Day22Test.java
@@ -0,0 +1,52 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day22Test {
+    private Day22 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day22();
+    }
+
+    @Test
+    void part1Case1() {
+        String data = """
+                      depth: 510
+                      target: 10,10
+                      """;
+
+        assertEquals(114, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(10204, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+
+    @Test
+    void part2Case1() {
+        String data = """
+                      depth: 510
+                      target: 10,10
+                      """;
+
+        assertEquals(45, testSubject.handlePart2(data.lines()));
+    }
+    
+    @Test
+    void testPart2Input() throws Exception {
+        //1309 too high
+        //997 too low
+        assertEquals(1004, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+}
