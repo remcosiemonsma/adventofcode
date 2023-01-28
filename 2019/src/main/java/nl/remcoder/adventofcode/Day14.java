66da72a2ea9e42cfa88c9180a3bd989e56a67b69
Remco Siemonsma <remco.siemonsma@sanoma.com>
2023-01-28 10:53:09 +0100

    Finished 2019 day 14

--- 2019/src/main/java/nl/remcoder/adventofcode/Day14.java
+++ 2019/src/main/java/nl/remcoder/adventofcode/Day14.java
@@ -1,26 +1,20 @@
 package nl.remcoder.adventofcode;
 
-import nl.remcoder.adventofcode.library.BiAdventOfCodeSolution;
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+import nl.remcoder.adventofcode.library.model.Pair;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Day14 implements BiAdventOfCodeSolution<Integer, Long> {
+public class Day14 implements AdventOfCodeSolution<Long> {
     @Override
-    public Integer handlePart1(Stream<String> input) {
+    public Long handlePart1(Stream<String> input) {
         var reactions = input.map(this::createReactionFromString)
                              .collect(Collectors.toMap(reaction -> reaction.chemical,
                                                        reaction -> reaction));
 
-        var fuelReaction = reactions.get("FUEL");
-
-        var waste = new HashMap<String, Long>();
-
-        return processReactionToOreNeeded(reactions, fuelReaction, waste);
+        return calculateRequiredOre(reactions, 1);
     }
 
     @Override
@@ -29,138 +23,58 @@ public class Day14 implements BiAdventOfCodeSolution<Integer, Long> {
                                                .collect(Collectors.toMap(reaction -> reaction.chemical,
                                                                          reaction -> reaction));
 
-        var amountOfOre = 1000000000000L;
+        var requiredOre = calculateRequiredOre(reactions, 1);
 
-        var fuelReaction = reactions.get("FUEL");
-
-        var waste = new HashMap<String, Long>();
-
-        var originalInput = new HashMap<>(fuelReaction.input);
-
-        var amountNeededForFuel = processReactionToOreNeeded(reactions, fuelReaction, waste);
-
-        fuelReaction.input = new HashMap<>(originalInput);
-
-        long amountFuelProduced = 0;
-
-        while (amountOfOre > amountNeededForFuel) {
-            var amountTimesReaction = amountOfOre / amountNeededForFuel;
-            System.out.println(amountTimesReaction);
-
-            amountFuelProduced += amountTimesReaction;
-            System.out.println(amountFuelProduced);
-
-            var totalWaste = new HashMap<>(waste);
-
-            totalWaste.replaceAll((s, amount) -> amount * amountTimesReaction);
-
-            flattenWasteToOre(totalWaste, reactions);
-
-            amountOfOre = totalWaste.get("ORE");
-
-            System.out.println(amountOfOre);
-        }
-
-        return amountFuelProduced;
-    }
-
-    private void flattenWasteToOre(Map<String, Long> waste,
-                                   Map<String, Reaction> reactions) {
-        var amountOre = 0L;
-
-        for (var chemical : waste.keySet()) {
-            var reaction = reactions.get(chemical);
-
-            var reactionTimes = waste.get(chemical) / reaction.amount;
-
-            var amountOreNeeded = processReactionToOreNeeded(reactions, reaction, new HashMap<>());
-
-            amountOre += amountOreNeeded * reactionTimes;
-        }
-
-        waste.clear();
-
-        waste.put("ORE", amountOre);
-    }
-
-    private int processReactionToOreNeeded(Map<String, Reaction> reactions, Reaction fuelReaction,
-                                           Map<String, Long> waste) {
-        var reactionFinished = false;
-
-        while (!reactionFinished) {
-            var newInput = new HashMap<String, Integer>();
-
-            for (var chemical : fuelReaction.input.keySet()) {
-                if ("ORE".equals(chemical)) {
-                    continue;
-                }
-
-                var amountNeeded = (long) fuelReaction.input.get(chemical);
-
-                if (waste.getOrDefault(chemical, 0L) >= amountNeeded) {
-                    var subtract = amountNeeded;
-                    waste.compute(chemical, (s, amount) -> {
-                        if (amount == null) {
-                            throw new AssertionError();
-                        }
-                        return amount - subtract;
-                    });
-                } else {
-                    amountNeeded -= waste.getOrDefault(chemical, 0L);
-                    waste.put(chemical, 0L);
-
-                    Reaction reaction = reactions.get(chemical);
-
-                    var reactionTimes = (int) Math.ceil((double) amountNeeded / (double) reaction.amount);
-
-                    var remainder = ((long) reaction.amount * reactionTimes) - amountNeeded;
-
-                    waste.compute(chemical, (s, amount) -> amount == null ? remainder : amount + remainder);
-
-                    for (var inputChemical : reaction.input.keySet()) {
-                        var amountRequired = reaction.input.get(inputChemical) * reactionTimes;
-                        newInput.compute(inputChemical,
-                                         (s, amount) -> amount == null ? amountRequired : amount + amountRequired);
-                    }
-                }
+        var target = 1000000000000L;
+        var lower = (target / requiredOre) - 1000;
+        var higher = (target / requiredOre) + 1000000000;
+        while (lower < higher)
+        {
+            long mid = (lower + higher) / 2;
+            long guess = calculateRequiredOre(reactions, mid);
+            if (guess > target)
+            {
+                higher = mid;
             }
-
-            if (fuelReaction.input.containsKey("ORE")) {
-                newInput.compute("ORE", (s, amount) -> amount == null ? fuelReaction.input.get("ORE") :
-                                                       amount + fuelReaction.input.get("ORE"));
+            else if (guess < target)
+            {
+                if (mid == lower) break;
+                lower = mid;
             }
-
-            fuelReaction.input = newInput;
-
-            if (fuelReaction.input.keySet().equals(Set.of("ORE"))) {
-                reactionFinished = true;
+            else
+            {
+                lower = mid;
+                break;
             }
         }
-
-        return fuelReaction.input.get("ORE");
+        
+        return lower;
     }
 
-    private Reaction createReactionFromString(String input) {
-        var split = input.split(" => ");
+    private Reaction createReactionFromString(String reactionString) {
+        var split = reactionString.split(" => ");
         var inputs = split[0].split(", ");
 
-        var reaction = new Reaction();
+        var input = Arrays.stream(inputs)
+                          .map(s -> s.split(" "))
+                          .collect(Collectors.toMap(strings -> strings[1],
+                                                    strings -> Integer.parseInt(strings[0])));
+        var chemical = split[1].split(" ")[1];
+        var amount = Integer.parseInt(split[1].split(" ")[0]);
 
-        reaction.input = Arrays.stream(inputs)
-                               .map(s -> s.split(" "))
-                               .collect(Collectors.toMap(strings -> strings[1],
-                                                         strings -> Integer.parseInt(strings[0])));
-
-        reaction.chemical = split[1].split(" ")[1];
-        reaction.amount = Integer.parseInt(split[1].split(" ")[0]);
-
-        return reaction;
+        return new Reaction(chemical, amount, input);
     }
 
     private static class Reaction {
         String chemical;
         int amount;
         Map<String, Integer> input;
+
+        public Reaction(String chemical, int amount, Map<String, Integer> input) {
+            this.chemical = chemical;
+            this.amount = amount;
+            this.input = input;
+        }
 
         @Override
         public String toString() {
@@ -170,5 +84,44 @@ public class Day14 implements BiAdventOfCodeSolution<Integer, Long> {
                    ", input=" + input +
                    '}';
         }
+    }
+
+    private long calculateRequiredOre(Map<String, Reaction> reactions, long fuelQuantity) {
+        var requiredOre = 0L;
+        var leftovers = new HashMap<String, Long>();
+        var queue = new LinkedList<Pair<String, Long>>();
+
+        queue.add(new Pair<>("FUEL", fuelQuantity));
+        while (!queue.isEmpty()) {
+            var element = queue.remove();
+            var requiredChemical = element.left();
+            var requiredQuantity = element.right();
+
+            if ("ORE".equals(requiredChemical)) {
+                requiredOre += requiredQuantity;
+                continue;
+            }
+
+            var leftoverQuantity = leftovers.getOrDefault(requiredChemical, 0L);
+            if (leftoverQuantity < requiredQuantity) {
+                leftovers.remove(requiredChemical);
+                var quantityToProduce = requiredQuantity - leftoverQuantity;
+                var reaction = reactions.get(requiredChemical);
+                var producedQuantity = reaction.amount;
+                var inputChemicals = reaction.input;
+
+                var batchesRequired = (quantityToProduce + producedQuantity - 1) / producedQuantity;
+                if (producedQuantity * batchesRequired > quantityToProduce) {
+                    leftovers.put(requiredChemical, (producedQuantity * batchesRequired) - quantityToProduce);
+                }
+                inputChemicals.forEach(
+                        (chemical, amount) -> queue.add(new Pair<>(chemical, (long) amount * batchesRequired)));
+            } else if (leftoverQuantity.equals(requiredQuantity)) {
+                leftovers.remove(requiredChemical);
+            } else {
+                leftovers.put(requiredChemical, leftoverQuantity - requiredQuantity);
+            }
+        }
+        return requiredOre;
     }
 }
---
+++ library/src/main/java/nl/remcoder/adventofcode/library/model/Pair.java
@@ -0,0 +1,4 @@
+package nl.remcoder.adventofcode.library.model;
+
+public record Pair<LEFT, RIGHT>(LEFT left, RIGHT right) {
+}
