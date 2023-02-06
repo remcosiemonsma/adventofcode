0e959faaf85a77c384996335f710729d58e24863
Remco Siemonsma <remco.siemonsma@sanoma.com>
2023-02-06 18:57:12 +0100

    Finished 2019 day 22

---
+++ 2019/src/main/java/nl/remcoder/adventofcode/Day22.java
@@ -0,0 +1,192 @@
+package nl.remcoder.adventofcode;
+
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day22 implements AdventOfCodeSolution<Long> {
+    @Override
+    public Long handlePart1(Stream<String> input) {
+        var techniques = input.flatMap(s -> toTechnique(s, 10007))
+                              .toList();
+        
+        return finalPositionForCard(2019, 10007, techniques);
+    }
+
+    @Override
+    public Long handlePart2(Stream<String> input) {
+        return solveB(input.toList());
+//        var deckSize = 119_315_717_514_047L;
+//        var repeats = 101_741_582_076_661L;
+//        var targetPosition = 2020L;
+//        
+//        var techniques = input.flatMap(s -> toTechnique(s, deckSize))
+//                              .toList();
+//
+//        var shufflesLeftUntilInitialState = deckSize - 1 - repeats;
+//
+//        var reduced = reduceShuffleProcess(deckSize, techniques);
+//        var repeated = repeatShuffleProcess(reduced, shufflesLeftUntilInitialState, deckSize);
+//        
+//        return finalPositionForCard(targetPosition, deckSize, repeated);
+    }
+    
+    private Stream<Technique> toTechnique(String operation, long deckSize) {
+        if (operation.startsWith("deal into")) {
+            return Stream.of(new Increment(deckSize - 1), new Cut(1));
+        } else if (operation.startsWith("deal with increment")) {
+            return Stream.of(new Increment(Integer.parseInt(operation.split(" ")[3])));
+        } else if (operation.startsWith("cut")) {
+            return Stream.of(new Cut(Integer.parseInt(operation.split(" ")[1])));
+        } else {
+            throw new AssertionError("Eek!");
+        }
+    }
+    
+    private long finalPositionForCard(long card, long deckSize, List<Technique> process) {
+        process = reduceShuffleProcess(deckSize, process);
+        
+        for (var technique : process) {
+            card = technique.nextPositionForCard(card, deckSize);
+        }
+        
+        return card;
+    }
+
+    private List<Technique> reduceShuffleProcess(long deckSize, List<Technique> initialProcess) {
+        var process = initialProcess;
+        while (process.size() > 2) {
+            var offset = 0;
+            while (offset < process.size() - 1) {
+                if (process.get(offset).canBeCombinedWith(process.get(offset + 1))) {
+                    var combined = process.get(offset).combine(process.get(offset + 1), deckSize);
+                    var newProcess = new ArrayList<Technique>();
+                    newProcess.addAll(process.subList(0, offset));
+                    newProcess.addAll(combined);
+                    newProcess.addAll(process.subList(offset + 2, process.size()));
+                    process = newProcess;
+                    offset = Math.max(0, offset - 1);
+                } else {
+                    offset++;
+                }
+            }
+        }
+        return process;
+    }
+
+    private List<Technique> repeatShuffleProcess(List<Technique> process, long times, long deckSize) {
+        var current = process;
+        char[] chars = reverse(Long.toString(times, 2).toCharArray());
+        var res = new ArrayList<Technique>();
+        for (var bit : chars) {
+            if (bit == '1') {
+                res.addAll(current);
+            }
+            var newCurrent = new ArrayList<Technique>();
+            newCurrent.addAll(current);
+            newCurrent.addAll(current);
+            current = reduceShuffleProcess(deckSize, newCurrent);
+        }
+        return reduceShuffleProcess(deckSize, res);
+    }
+
+    private char[] reverse (char[] data) {
+        char[] result = new char[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            char c = data[data.length - i - 1];
+            if (c == '0') {
+                c = '1';
+            } else {
+                c = '0';
+            }
+            result[i] = c;
+        }
+
+        return result;
+    }
+
+    private sealed abstract static class Technique {
+        abstract long nextPositionForCard(long card, long deckSize);
+
+        private long mulMod(long a, long b, long mod) {
+            return BigInteger.valueOf(a).multiply(BigInteger.valueOf(b)).mod(BigInteger.valueOf(mod)).longValueExact();
+        }
+
+        public List<Technique> combine(Technique other, long deckSize) {
+            if (this instanceof Cut thisCut && other instanceof Cut otherCut) {
+                return List.of(new Cut(Math.floorMod(thisCut.value + otherCut.value, deckSize)));
+            } else if (this instanceof Increment thisIncrement && other instanceof Increment otherIncrement) {
+                return List.of(new Increment(mulMod(thisIncrement.value, otherIncrement.value, deckSize)));
+            } else if (this instanceof Cut cut && other instanceof Increment increment) {
+                return List.of(new Increment(increment.value), new Cut(mulMod(cut.value, increment.value, deckSize)));
+            } else {
+                throw new AssertionError("Eek!");
+            }
+        }
+
+        public boolean canBeCombinedWith(Technique other) {
+            return !(this instanceof Increment && other instanceof Cut);
+        }
+    }
+    
+    private static final class Cut extends Technique {
+        private final long value;
+
+        private Cut(long value) {
+            this.value = value;
+        }
+
+        @Override
+        long nextPositionForCard(long card, long deckSize) {
+            return Math.floorMod(card - value, deckSize);
+        }
+    }
+    
+    private static final class Increment extends Technique {
+        private final long value;
+
+        private Increment(long value) {
+            this.value = value;
+        }
+
+        @Override
+        long nextPositionForCard(long card, long deckSize) {
+            return Math.floorMod(card * value, deckSize);
+        }
+    }
+
+    private long solveB(List<String> input) {
+        // Had to look at reddit: https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/
+        long m = 119315717514047L;
+        long n = 101741582076661L;
+        long pos = 2020;
+
+        long a = 1;
+        long b = 0;
+        for (String operation : input) {
+            if ("deal into new stack".equals(operation)) {
+                a = -a % m;
+                b = (m - 1 - b) % m;
+            } else if (operation.startsWith("deal")) {
+                long increment = Long.parseLong(operation.substring(operation.lastIndexOf(' ') + 1));
+                a = (a * increment) % m;
+                b = (b * increment) % m;
+            } else { // cut
+                long cut = Long.parseLong(operation.substring(operation.lastIndexOf(' ') + 1));
+                b = (b - cut) % m;
+            }
+        }
+
+        // r = (b * pow(1-a, m-2, m)) % m
+        BigInteger mm = BigInteger.valueOf(m);
+        BigInteger r = BigInteger.valueOf(b).multiply(BigInteger.valueOf(1 - a).modPow(BigInteger.valueOf(m - 2), mm)).mod(BigInteger.valueOf(m));
+
+        // return ((pos - r) * pow(a, n*(m-2), m) + r) % m
+        return BigInteger.valueOf(pos - r.longValue()).multiply(BigInteger.valueOf(a).modPow(BigInteger.valueOf(n).multiply(BigInteger.valueOf(m - 2)), mm)).add(r)
+                         .mod(BigInteger.valueOf(m)).longValue();
+    }
+}
---
+++ 2019/src/test/java/nl/remcoder/adventofcode/Day22Test.java
@@ -0,0 +1,30 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day22Test {
+    private Day22 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day22();
+    }
+    
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(3143, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(1140147758, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+}
