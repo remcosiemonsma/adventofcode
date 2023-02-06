1382f66b9a750ab6019fd1581fa8505f37545dc9
Remco Siemonsma <remco.siemonsma@sanoma.com>
2023-02-06 22:44:47 +0100

    Finished 2019 day 25

---
+++ 2019/src/main/java/nl/remcoder/adventofcode/Day25.java
@@ -0,0 +1,43 @@
+package nl.remcoder.adventofcode;
+
+import nl.remcoder.adventofcode.intcodecomputer.*;
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Day25 implements AdventOfCodeSolution<String> {
+    public static void main(String[] args) throws Exception {
+        var day25 = new Day25();
+        day25.handlePart1(Files.lines(Paths.get(ClassLoader.getSystemResource("day25/input").toURI())));
+    }
+    @Override
+    public String handlePart1(Stream<String> input) throws Exception {
+        var opcodes = input.flatMap(s -> Arrays.stream(s.split(",")))
+                           .mapToLong(Long::parseLong)
+                           .toArray();
+        
+        var inputQueue = new ProducingQueue(() -> {
+            try {
+                return System.in.read();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        var outputConsumer = new ConsumingQueue(outputValue -> System.out.print((char) outputValue.intValue()));
+        
+        var intcodeComputer = new IntCodeComputer(opcodes, inputQueue, outputConsumer);
+        
+        intcodeComputer.runProgram();
+        
+        return null;
+    }
+
+    @Override
+    public String handlePart2(Stream<String> input) throws Exception {
+        return "Merry Christmas";
+    }
+}
