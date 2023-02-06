fe3cc2e37dd27ac0dfbed2c1cdd3dca14e51cb1a
Remco Siemonsma <remco.siemonsma@sanoma.com>
2023-02-06 22:44:47 +0100

    Finished 2019 day 25

---
+++ 2019/src/test/java/nl/remcoder/adventofcode/Day25Test.java
@@ -0,0 +1,24 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day25Test {
+    private Day25 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day25();
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals("25166400", testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day25/input").toURI()))));
+    }
+}
