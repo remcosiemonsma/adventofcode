e28c78c36020e8c9eca6ac50eefa6f762082a5b9
Remco Siemonsma <remco.siemonsma@gmail.com>
2023-12-01 12:01:48 +0100

    Finished 2023 day 1

---
+++ 2023/src/main/java/nl/remcoder/adventofcode/Day1.java
@@ -0,0 +1,90 @@
+package nl.remcoder.adventofcode;
+
+import nl.remcoder.adventofcode.library.AdventOfCodeSolution;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Day1 implements AdventOfCodeSolution<Integer> {
+    private final Pattern NUMBERS =
+            Pattern.compile("(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)");
+
+    @Override
+    public Integer handlePart1(Stream<String> input) {
+        return input.mapToInt(this::getCalibrationValue)
+                    .sum();
+    }
+
+    @Override
+    public Integer handlePart2(Stream<String> input) {
+        return input.map(NUMBERS::matcher)
+                    .mapToInt(this::getCalibrationValue2)
+                    .sum();
+    }
+
+    private int getCalibrationValue2(Matcher matcher) {
+        var numbers = new ArrayList<String>();
+
+        var position = 0;
+
+        while (matcher.find(position)) {
+            numbers.add(matcher.group(1));
+            position = matcher.start() + 1;
+        }
+
+        var firstDigit = stringToNumber(numbers.get(0));
+        var secondDigit = stringToNumber(numbers.get(numbers.size() - 1));
+
+        return firstDigit * 10 + secondDigit;
+    }
+
+    private int stringToNumber(String s) {
+        return switch (s) {
+            case "1", "one" -> 1;
+            case "2", "two" -> 2;
+            case "3", "three" -> 3;
+            case "4", "four" -> 4;
+            case "5", "five" -> 5;
+            case "6", "six" -> 6;
+            case "7", "seven" -> 7;
+            case "8", "eight" -> 8;
+            case "9", "nine" -> 9;
+            default -> throw new AssertionError("Eek!");
+        };
+    }
+
+    private int getCalibrationValue(String s) {
+        var firstDigit = findFirstDigit(s);
+        var secondDigit = findLastDigit(s);
+
+        return firstDigit * 10 + secondDigit;
+    }
+
+    private int findFirstDigit(String s) {
+        var position = 0;
+
+        while (position < s.length()) {
+            if (Character.isDigit(s.charAt(position))) {
+                return s.charAt(position) - '0';
+            }
+            position++;
+        }
+
+        throw new AssertionError("Eek!");
+    }
+
+    private int findLastDigit(String s) {
+        var position = s.length() - 1;
+
+        while (position >= 0) {
+            if (Character.isDigit(s.charAt(position))) {
+                return s.charAt(position) - '0';
+            }
+            position--;
+        }
+
+        return -1;
+    }
+}
---
+++ 2023/src/test/java/nl/remcoder/adventofcode/Day1Test.java
@@ -0,0 +1,57 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day1Test {
+    private Day1 testSubject;
+
+    @BeforeEach
+    public void setUp() {
+        testSubject = new Day1();
+    }
+
+    @Test
+    public void part1Case1() {
+        String data = """
+                      1abc2
+                      pqr3stu8vwx
+                      a1b2c3d4e5f
+                      treb7uchet
+                      """;
+
+        assertEquals(142, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    public void testPart1Input() throws Exception {
+        assertEquals(55017, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day1/input").toURI()))));
+    }
+
+    @Test
+    public void part2Case1() {
+        String data = """
+                      two1nine
+                      eightwothree
+                      abcone2threexyz
+                      xtwone3four
+                      4nineeightseven2
+                      zoneight234
+                      7pqrstsixteen
+                      """;
+
+        assertEquals(281, testSubject.handlePart2(data.lines()));
+    }
+
+    @Test
+    public void testPart2Input() throws Exception {
+        assertEquals(53539, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day1/input").toURI()))));
+    }
+}
