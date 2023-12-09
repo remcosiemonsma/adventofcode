1cb108ca6058a943f08f43312cfceb731b77d4e0
Remco Siemonsma <remco.siemonsma@gmail.com>
2023-12-09 08:19:07 +0100

    Finished 2023 day 9

---
+++ 2023/src/test/java/nl/remcoder/adventofcode/Day9Test.java
@@ -0,0 +1,52 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day9Test {
+    private Day9 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day9();
+    }
+
+    @Test
+    void part1Case1() {
+        String data = """
+                      0 3 6 9 12 15
+                      1 3 6 10 15 21
+                      10 13 16 21 30 45
+                      """;
+
+        assertEquals(114, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(1938731307, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day9/input").toURI()))));
+    }
+
+    @Test
+    void part1Case2() {
+        String data = """
+                      0 3 6 9 12 15
+                      1 3 6 10 15 21
+                      10 13 16 21 30 45
+                      """;
+
+        assertEquals(2, testSubject.handlePart2(data.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(948, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day9/input").toURI()))));
+    }
+}
