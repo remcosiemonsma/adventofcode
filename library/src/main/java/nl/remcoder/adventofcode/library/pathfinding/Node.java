35a5aab2b10ff30ad95b83935ecbfd0dea88ad4a
Remco Siemonsma <remco.siemonsma@gmail.com>
2023-12-16 09:45:57 +0100

    Code cleanup

--- library/src/main/java/nl/remcoder/adventofcode/library/pathfinding/Node.java
+++ library/src/main/java/nl/remcoder/adventofcode/library/pathfinding/Node.java
@@ -2,7 +2,7 @@ package nl.remcoder.adventofcode.library.pathfinding;
 
 import java.util.Map;
 
-public abstract class Node<T extends Node<?>> implements Comparable<T> {
+public abstract class Node<T extends Node<T>> implements Comparable<T> {
     private long distance = Long.MAX_VALUE;
     private boolean visited;
     public abstract Map<T, Long> getNeighbors();
