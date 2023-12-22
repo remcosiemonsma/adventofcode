4d29d15fc597afd54566678cfa12f060d85ccada
Remco Siemonsma <remco.siemonsma@gmail.com>
2023-12-22 09:03:21 +0100

    Finished 2023 day 22

---
+++ 2023/src/test/java/nl/remcoder/adventofcode/Day22Test.java
@@ -0,0 +1,60 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day22Test {
+    private Day22 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day22();
+    }
+
+    @Test
+    void part1Case1() {
+        String data = """
+                      1,0,1~1,2,1
+                      0,0,2~2,0,2
+                      0,2,3~2,2,3
+                      0,0,4~0,2,4
+                      2,0,5~2,2,5
+                      0,1,6~2,1,6
+                      1,1,8~1,1,9
+                      """;
+
+        assertEquals(5, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(515, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+
+    @Test
+    void part1Case2() {
+        String data = """
+                      1,0,1~1,2,1
+                      0,0,2~2,0,2
+                      0,2,3~2,2,3
+                      0,0,4~0,2,4
+                      2,0,5~2,2,5
+                      0,1,6~2,1,6
+                      1,1,8~1,1,9
+                      """;
+
+        assertEquals(7, testSubject.handlePart2(data.lines()));
+    }
+
+    @Test
+    void testPart2Input() throws Exception {
+        assertEquals(101541, testSubject.handlePart2(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day22/input").toURI()))));
+    }
+}
--- library/src/main/java/nl/remcoder/adventofcode/library/model/Coordinate3D.java
+++ library/src/main/java/nl/remcoder/adventofcode/library/model/Coordinate3D.java
@@ -1,5 +1,7 @@
 package nl.remcoder.adventofcode.library.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public record Coordinate3D(int x, int y, int z) {
@@ -35,7 +37,47 @@ public record Coordinate3D(int x, int y, int z) {
     public int getDistanceTo(Coordinate3D other) {
         return Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(z - other.z);
     }
-    
+
+    public List<Coordinate3D> getAllBetween(Coordinate3D other) {
+        var coordinates = new ArrayList<Coordinate3D>();
+
+        if (this.x == other.x && this.y == other.y) {
+            if (z <= other.z) {
+                for (int z = this.z; z <= other.z; z++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            } else {
+                for (int z = other.z; z <= this.z; z++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            }
+        } else if (this.x == other.x && this.z == other.z) {
+            if (y <= other.y) {
+                for (int y = this.y; y <= other.y; y++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            } else {
+                for (int y = other.y; y <= this.y; y++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            }
+        } else if (this.y == other.y && this.z == other.z) {
+            if (x <= other.x) {
+                for (int x = this.x; x <= other.x; x++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            } else {
+                for (int x = other.x; x <= this.x; x++) {
+                    coordinates.add(new Coordinate3D(x, y, z));
+                }
+            }
+        } else {
+            throw new UnsupportedOperationException("Diagonal lines are not supported!");
+        }
+
+        return coordinates;
+    }
+
     @Override
     public String toString() {
         return x + "," + y + "," + z;
