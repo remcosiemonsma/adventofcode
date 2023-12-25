b698af00c166d1d8de066491a82659ad749ba5f7
Remco Siemonsma <remco.siemonsma@gmail.com>
2023-12-25 10:02:50 +0100

    Finished 2023 day 25

---
+++ 2023/src/test/java/nl/remcoder/adventofcode/Day25Test.java
@@ -0,0 +1,45 @@
+package nl.remcoder.adventofcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day25Test {
+    private Day25 testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new Day25();
+    }
+
+    @Test
+    void part1Case1() {
+        String data = """
+                      jqt: rhn xhk nvd
+                      rsh: frs pzl lsr
+                      xhk: hfx
+                      cmg: qnr nvd lhk bvb
+                      rhn: xhk bvb hfx
+                      bvb: xhk hfx
+                      pzl: lsr hfx nvd
+                      qnr: nvd
+                      ntq: jqt hfx bvb xhk
+                      nvd: lhk
+                      lsr: lhk
+                      rzs: qnr cmg lsr rsh
+                      frs: qnr lhk lsr
+                      """;
+
+        assertEquals(54, testSubject.handlePart1(data.lines()));
+    }
+
+    @Test
+    void testPart1Input() throws Exception {
+        assertEquals(614655, testSubject.handlePart1(
+                Files.lines(Paths.get(ClassLoader.getSystemResource("day25/input").toURI()))));
+    }
+}
--- library/src/main/java/nl/remcoder/adventofcode/library/model/Pair.java
+++ library/src/main/java/nl/remcoder/adventofcode/library/model/Pair.java
@@ -1,4 +1,23 @@
 package nl.remcoder.adventofcode.library.model;
 
+import java.util.Objects;
+
 public record Pair<LEFT, RIGHT>(LEFT left, RIGHT right) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return (Objects.equals(left, pair.left) && Objects.equals(right, pair.right)) ||
+               (Objects.equals(left, pair.right) && Objects.equals(right, pair.left));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
 }
