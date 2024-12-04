90f16258e4d71c268d6f059860dcf411637f9f87
Remco Siemonsma <remco.siemonsma@gmail.com>
2024-12-04 08:25:02 +0100

    Added support for diagonals to direction

--- library/src/main/java/nl/remcoder/adventofcode/library/model/Direction.java
+++ library/src/main/java/nl/remcoder/adventofcode/library/model/Direction.java
@@ -2,34 +2,50 @@ package nl.remcoder.adventofcode.library.model;
 
 public enum Direction {
     UP,
+    UPLEFT,
     LEFT,
+    DOWNLEFT,
     DOWN,
-    RIGHT;
+    DOWNRIGHT,
+    RIGHT,
+    UPRIGHT,;
 
     public Direction rotateClockWise() {
         return switch (this) {
             case UP -> RIGHT;
-            case RIGHT -> DOWN;
-            case DOWN -> LEFT;
+            case UPLEFT -> DOWNLEFT;
             case LEFT -> UP;
+            case DOWNLEFT -> DOWNRIGHT;
+            case RIGHT -> DOWN;
+            case DOWNRIGHT -> UPRIGHT;
+            case DOWN -> LEFT;
+            case UPRIGHT -> UPLEFT;
         };
     }
 
     public Direction rotateCounterClockWise() {
         return switch (this) {
             case UP -> LEFT;
+            case UPLEFT -> UPRIGHT;
             case LEFT -> DOWN;
+            case DOWNLEFT -> UPLEFT;
             case DOWN -> RIGHT;
+            case DOWNRIGHT -> DOWNLEFT;
             case RIGHT -> UP;
+            case UPRIGHT -> DOWNRIGHT;
         };
     }
     
     public Direction flip() {
         return switch (this) {
             case UP -> DOWN;
-            case DOWN -> UP;
-            case RIGHT -> LEFT;
+            case UPLEFT -> DOWNLEFT;
             case LEFT -> RIGHT;
+            case DOWNLEFT -> UPLEFT;
+            case DOWN -> UP;
+            case DOWNRIGHT -> UPRIGHT;
+            case RIGHT -> LEFT;
+            case UPRIGHT -> DOWNRIGHT;
         };
     }
 }
