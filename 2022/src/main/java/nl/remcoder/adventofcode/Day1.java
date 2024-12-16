d7f004946ade626b54ba9531e9e220d1e015da80
Remco Siemonsma <remco.siemonsma@gmail.com>
2024-12-16 08:26:07 +0100

    Small improvement

--- 2022/src/main/java/nl/remcoder/adventofcode/Day1.java
+++ 2022/src/main/java/nl/remcoder/adventofcode/Day1.java
@@ -11,9 +11,10 @@ public class Day1 implements AdventOfCodeSolution<Integer> {
     public Integer handlePart1(Stream<String> input) {
         return input.collect(new CombiningCollector<>(Integer::parseInt, String::isBlank, BagImpl::new))
                     .parallel()
-                    .mapToInt(bag -> ((BagImpl) bag).stream()
-                                                    .mapToInt(Integer::intValue)
-                                                    .sum())
+                    .map(BagImpl.class::cast)
+                    .mapToInt(bag -> bag.stream()
+                                        .mapToInt(Integer::intValue)
+                                        .sum())
                     .max()
                     .orElseThrow(() -> new AssertionError("Eek!"));
     }
@@ -22,9 +23,10 @@ public class Day1 implements AdventOfCodeSolution<Integer> {
     public Integer handlePart2(Stream<String> input) {
         return input.collect(new CombiningCollector<>(Integer::parseInt, String::isBlank, BagImpl::new))
                     .parallel()
-                    .map(bag -> ((BagImpl) bag).stream()
-                                               .mapToInt(Integer::intValue)
-                                               .sum())
+                    .map(BagImpl.class::cast)
+                    .map(bag -> bag.stream()
+                                   .mapToInt(Integer::intValue)
+                                   .sum())
                     .sorted(Comparator.reverseOrder())
                     .limit(3)
                     .mapToInt(Integer::intValue)
